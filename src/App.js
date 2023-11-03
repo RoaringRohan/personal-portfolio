954464cd73e3d92c0360e0cd846d28d79afb21fc
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2023-11-02 20:29:48 -0400

    Added about component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Experience from './components/Experience';
+import About from './components/About';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
         <Route index path="/" element={<Home />} />
         <Route index path="experience" element={<Experience />} />
+        <Route index path="about" element={<About />} />
     </Routes>
     </>
   );
---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const About = () => {
+
+    return (
+        <div className="container experience">
+            <div className="text-zone">
+                <h1>About</h1>
+            </div>
+        </div>
+    );
+}
+
+export default About;
