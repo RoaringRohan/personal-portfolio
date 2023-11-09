16c625b420924159f35316d2325c8b83d29091cc
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2023-11-08 20:01:20 -0500

    Added Layout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import './App.scss';
-import Nav from './components/Nav';
+import Layout from './components/Layout';
 import Home from './components/Home';
 import Experience from './components/Experience';
 import About from './components/About';
@@ -11,14 +11,15 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-    <Nav />
-    <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route index path="experience" element={<Experience />} />
-        <Route index path="about" element={<About />} />
-        <Route index path="hobbies" element={<Hobbies />} />
-        <Route index path="contact" element={<Contact />} />
-    </Routes>
+    <Layout>
+      <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="about" element={<About />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="contact" element={<Contact />} />
+      </Routes>
+    </Layout>
     </>
   );
 }
---
+++ src/components/Layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../Nav';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Nav />
+            <div className="content">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
