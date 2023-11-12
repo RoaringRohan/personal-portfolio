1a5e61566f1cafdd380f9023f674c5420a9b46dd
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2023-11-11 23:00:54 -0500

    Added in Projects component and formatted it to the page.

--- src/components/Home/Projects/index.js
+++ src/components/Home/Projects/index.js
@@ -3,52 +3,53 @@ import React from 'react';
 
 const Projects = () => {
     return (
-        <div class="accordion">
-
-            <div class="box a1">
-                <div class="image_1">
-                    <div class="text">
-                        <h2>Lorem Ipsum</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Vestibulum iaculis nisl sed dictum aliquam.
-                        </p>
+        <div className="project-container">
+            <div class="accordion">
+                <div class="box a1">
+                    <div class="image_1">
+                        <div class="text">
+                            <h2>Lorem Ipsum</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Vestibulum iaculis nisl sed dictum aliquam.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="box a2">
-                <div class="image_2">
-                    <div class="text">
-                        <h2>Lorem Ipsum</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Vestibulum iaculis nisl sed dictum aliquam.
-                        </p>
+                <div class="box a2">
+                    <div class="image_2">
+                        <div class="text">
+                            <h2>Lorem Ipsum</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Vestibulum iaculis nisl sed dictum aliquam.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="box a3">
-                <div class="image_3">
-                    <div class="text">
-                        <h2>Lorem Ipsum</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Vestibulum iaculis nisl sed dictum aliquam.
-                        </p>
+                <div class="box a3">
+                    <div class="image_3">
+                        <div class="text">
+                            <h2>Lorem Ipsum</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Vestibulum iaculis nisl sed dictum aliquam.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="box a4">
-                <div class="image_4">
-                    <div class="text">
-                        <h2>Lorem Ipsum</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Vestibulum iaculis nisl sed dictum aliquam.
-                        </p>
+                <div class="box a4">
+                    <div class="image_4">
+                        <div class="text">
+                            <h2>Lorem Ipsum</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Vestibulum iaculis nisl sed dictum aliquam.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,34 @@
 import './index.scss';
 //import Portrait from './Portrait';
-//import Projects from './Projects';
+import Projects from './Projects';
 
 const Home = () => {
-
     return (
         <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span>Hey,</span>
-                    <br />
-                    <span>I'm Rohan!</span>
-                </h1>
-                <h2>Software Engineer / Full-Stack Developer</h2>
-                <p>
-                    Here's all the projects that I've worked on in the past.
-                </p>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span>Hey,</span>
+                        <br />
+                        <span>I'm Rohan!</span>
+                    </h1>
+                    <h2>Software Engineer / Full-Stack Developer</h2>
+                </div>
+                <div className="project-intro">
+                    <p>
+                        Here's all the projects that I've worked on in the past.
+                    </p>
+                </div>
+                <div className="project-accordion">
+                    <Projects />
+                </div>
             </div>
-            <div className="gap">
-
-            </div>
-        </div>
         </>
     );
 }
 
 export default Home;
+
 
 // Insert this when ready to start working on profile pic in homepage
 //<Portrait />
