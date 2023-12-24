8577a113e424e52b62fbdb9d35561752cf489d7b
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2023-12-23 21:14:25 -0500

    Adding logic to submit button on contact page

--- src/components/Contact/Form/index.js
+++ src/components/Contact/Form/index.js
@@ -1,33 +1,113 @@
+import React, { useState } from 'react';
 import './index.scss';
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-    return (
-        <div className="container form">
-            <div className="form">
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" className="name-input" />
-                    </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" className="email-input" />
-                    </div>
+  const publicKey = "uZ-p9m2sS5clLYbqG";
+  const serviceID = "service_m7xtoim";
+  const templateID = "template_65vdua9";
 
-                    <div className="form-group">
-                        <label htmlFor="message">Message:</label>
-                        <textarea id="message" className="message-input"></textarea>
-                    </div>
+  emailjs.init(publicKey);
 
-                    <div className="form-group">
-                        <button type="submit" className="submit-button">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send formData to your server or perform any necessary actions
+    emailjs.send(serviceID, templateID, formData)
+      .then(() => {
+        console.log('Form data submitted:', formData);
+
+        // Change the submit button text to "Sent!"
+        const submitButton = document.querySelector('.submit-button');
+        if (submitButton) {
+          submitButton.innerText = 'Sent!';
+        }
+
+        // Delay the page refresh by 5 seconds
+        setTimeout(() => {
+          // Refresh the page
+          window.location.reload();
+        }, 5000);
+      })
+      .catch((error) => {
+        console.log('Something went wrong:', error);
+
+        // Change the submit button text to "Something went wrong"
+        const submitButton = document.querySelector('.submit-button');
+        if (submitButton) {
+          submitButton.innerText = 'Something went wrong';
+        }
+
+        // Delay the page refresh by 5 seconds
+        setTimeout(() => {
+          // Refresh the page
+          window.location.reload();
+        }, 5000);
+      });
+  };
+
+  return (
+    <div className="container form">
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              className="name-input"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              className="email-input"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              className="message-input"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Form;
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,19 @@
+import React from 'react';
 import './index.scss';
 import Form from './Form';
 
 const Contact = () => {
-
-    return (
-        <div className="container contact">
-            <div className="text-zone-1">
-                <h1>Let's get in touch!</h1>
-            </div>
-            <div className="text-zone-2">
-                <h2>Whether you'd like to contact me for inquiries or to see some of my projects or even just to talk I'm here to respond.</h2>
-            </div>
-            <div className="input">
-                <Form />
-            </div>
-        </div>
-    );
+  return (
+    <div className="container contact">
+      <div className="text-zone">
+        <h1>Let's get in touch!</h1>
+        <h2>Whether you'd like to contact me for inquiries or to see some of my projects or even just to talk I'm here to respond.</h2>
+      </div>
+      <div className="input">
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
