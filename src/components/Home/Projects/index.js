c8cc058c65119839b6a283769e36d1433eaa50d2
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2023-12-24 21:49:24 -0500

    Redid the Projects component for homepage, also added dedicated project pictures.

--- src/components/Home/Projects/index.js
+++ src/components/Home/Projects/index.js
@@ -1,51 +1,41 @@
+// index.js
 import './index.scss';
 import React from 'react';
 
 const Projects = () => {
     return (
         <div className="container project">
-            <div class="accordion">
-                <div class="box a1">
-                    <div class="image_1">
-                        <div class="text">
-                            <h2>Music Playlist Application</h2>
-                            <p>
-                                Created a full-stack application that allows users to create playlists using a NoSQL backend with Angular as the front.
-                            </p>
-                        </div>
+            <div className="project-item project-one">
+                <div className="project-text">
+                    <div className="project-text-content">
+                        <p>Project One Description</p>
                     </div>
                 </div>
+            </div>
+            <div className="project-gap"></div>
 
-                <div class="box a2">
-                    <div class="image_2">
-                        <div class="text">
-                            <h2>Course Outline Management Application</h2>
-                            <p>
-                                Collaborated with a 3 member team to make a full-stack course management system which utilized Firebase as back-end and Google Compute Engine for deployment.
-                            </p>
-                        </div>
+            <div className="project-item project-two">
+                <div className="project-text">
+                    <div className="project-text-content">
+                        <p>Project Two Description</p>
                     </div>
                 </div>
+            </div>
+            <div className="project-gap"></div>
 
-                <div class="box a3">
-                    <div class="image_3">
-                        <div class="text">
-                            <h2>Service Ticket Management Application</h2>
-                            <p>
-                                Created an internal ticket management application to manage technical service and support for companies using a REST API and MySQL database.
-                            </p>
-                        </div>
+            <div className="project-item project-three">
+                <div className="project-text">
+                    <div className="project-text-content">
+                        <p>Project Three Description</p>
                     </div>
                 </div>
+            </div>
+            <div className="project-gap"></div>
 
-                <div class="box a4">
-                    <div class="image_4">
-                        <div class="text">
-                            <h2>Image Recognition Application</h2>
-                            <p>
-                                Created and tested a neural network for hand-written number recognition using Python.
-                            </p>
-                        </div>
+            <div className="project-item project-four">
+                <div className="project-text">
+                    <div className="project-text-content">
+                        <p>Project Four Description</p>
                     </div>
                 </div>
             </div>
